3c2e8f53c2af195c930ab9eb1b85c6dd1b5be2ee
dom111 <dom111@users.noreply.github.com>
2021-02-14 15:31:00 +0000

    Updates to use shard `simpleRLELoader` from `simple-world-generator`

--- tests/BasePathFinder.test.ts
+++ tests/BasePathFinder.test.ts
@@ -1,126 +1,22 @@
-import {
-  Arctic,
-  Desert,
-  Forest,
-  Grassland,
-  Hills,
-  Jungle,
-  Mountains,
-  Ocean,
-  Plains,
-  River,
-  Swamp,
-  Tundra,
-} from '@civ-clone/civ1-world/Terrains';
-import {
-  Coal,
-  Fish,
-  Game,
-  Gems,
-  Gold,
-  Horse,
-  Oasis,
-  Oil,
-  Seal,
-  Shield,
-} from '@civ-clone/civ1-world/TerrainFeatures';
-import {
-  RuleRegistry,
-  instance as ruleRegistryInstance,
-} from '@civ-clone/core-rule/RuleRegistry';
+import RuleRegistry from '@civ-clone/core-rule/RuleRegistry';
 import BasePathFinder from '../BasePathFinder';
 import City from '@civ-clone/core-city/City';
 import CityRegistry from '@civ-clone/core-city/CityRegistry';
-import Loader from '@civ-clone/simple-world-generator/tests/lib/Loader';
 import Path from '@civ-clone/core-world-path/Path';
 import Player from '@civ-clone/core-player/Player';
-import Terrain from '@civ-clone/core-terrain/Terrain';
-import TerrainFeature from '@civ-clone/core-terrain-feature/TerrainFeature';
 import TileImprovementRegistry from '@civ-clone/core-tile-improvement/TileImprovementRegistry';
 import TransportRegistry from '@civ-clone/core-unit-transport/TransportRegistry';
 import UnitImprovementRegistry from '@civ-clone/core-unit-improvement/UnitImprovementRegistry';
 import UnitRegistry from '@civ-clone/core-unit/UnitRegistry';
 import { Warrior } from '@civ-clone/civ1-unit/Units';
-import World from '@civ-clone/core-world/World';
 import action from '@civ-clone/civ1-unit/Rules/Unit/action';
 import created from '@civ-clone/civ1-unit/Rules/Unit/created';
 import { expect } from 'chai';
 import moved from '@civ-clone/civ1-unit/Rules/Unit/moved';
 import movementCost from '@civ-clone/civ1-unit/Rules/Unit/movementCost';
+import simpleRLELoader from '@civ-clone/simple-world-generator/tests/lib/simpleRLELoader';
 import unitYield from '@civ-clone/civ1-unit/Rules/Unit/yield';
 import validateMove from '@civ-clone/civ1-unit/Rules/Unit/validateMove';
-
-type StringTerrain =
-  | 'A'
-  | 'D'
-  | 'F'
-  | 'G'
-  | 'H'
-  | 'J'
-  | 'M'
-  | 'O'
-  | 'P'
-  | 'R'
-  | 'S'
-  | 'T';
-type StringFeature = 'c' | 'f' | 'a' | 'e' | 'g' | 'h' | 'i' | 'o' | 's' | 'd';
-
-export const simpleRLELoader = (
-  ruleRegistry: RuleRegistry = ruleRegistryInstance
-) => (map: string, height: number, width: number): World => {
-  const terrainLookup = {
-      A: Arctic,
-      D: Desert,
-      F: Forest,
-      G: Grassland,
-      H: Hills,
-      J: Jungle,
-      M: Mountains,
-      O: Ocean,
-      P: Plains,
-      R: River,
-      S: Swamp,
-      T: Tundra,
-    } as { [key in StringTerrain]: typeof Terrain },
-    featureLookup = {
-      c: Coal,
-      f: Fish,
-      a: Game,
-      e: Gems,
-      g: Gold,
-      h: Horse,
-      i: Oasis,
-      o: Oil,
-      s: Seal,
-      d: Shield,
-    } as { [key in StringFeature]: typeof TerrainFeature },
-    data = (
-      map.replace(/\s+/g, '').match(/(\d+|)([A-Z])([a-z]+|)/g) || []
-    ).flatMap((definition: string): [Terrain, ...TerrainFeature[]][] => {
-      const [, n, terrainIndex, featureIndices] = definition.match(
-        /(\d+|)([A-Z])([a-z]+|)?/
-      ) as [never, string, StringTerrain, StringFeature[]];
-
-      return new Array(parseInt(n) || 1).fill(0).map((): [
-        Terrain,
-        ...TerrainFeature[]
-      ] => {
-        const terrain = new terrainLookup[terrainIndex](),
-          features =
-            [...(featureIndices || [])].map(
-              (featureIndex: StringFeature): TerrainFeature =>
-                new featureLookup[featureIndex](terrain)
-            ) || [];
-
-        return [terrain, ...features];
-      });
-    }),
-    world = new World(new Loader(height, width, data));
-
-  world.build(ruleRegistry);
-
-  return world;
-};
 
 describe('BasePathFinder', () => {
   const ruleRegistry = new RuleRegistry(),
